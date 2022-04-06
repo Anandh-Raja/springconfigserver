dd15aea4ff0d9504780b6560f8af59e5c9def93a
K Anandh Raja Raja (APMEA - iDEAS-Cloud Transformation) <ka20181794@wipro.com>
2022-04-06 17:54:43 +0530

    first commit

---
+++ src/main/java/com/wipro/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.wipro.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableConfigServer
+
+
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+}
